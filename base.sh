52b8e6a349f9284c637c315679d1a49178701535
Xia Huang <60800828+caiqingwen@users.noreply.github.com>
2025-07-19 10:21:40 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-name="Alice"
-age=10
-echo "my name is ${name}, I am ${age} years old"
+echo "脚本名称：$0"
+echo "第一个参数：$1"
+echo "第二个参数：$2"
+echo "参数个数：$#"
+echo "上一个命令的退出状态：$?"
+echo "当前脚本的进程ID：$$"
+
+echo "所有参数：\$@"
+for arg in "$@"; do
+    echo "参数：$arg"
+done
+
+echo "所有参数（作为单一字符串）：\$*"
+for arg in "$*"; do
+    echo "参数：$arg"
+done
